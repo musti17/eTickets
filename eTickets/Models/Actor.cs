834b237ff9069cc9e10573451eab47fd30e20fcc
musti17 <mustafazahid167@gmail.com>
2023-06-18 11:30:24 +0500

    Added Actor Model

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,11 @@
+﻿namespace eTickets.Models
+{
+    public class Actor
+    {
+        public string ProfilePictureURL { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
