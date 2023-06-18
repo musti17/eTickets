24d34d72b75c5fad0099a7ef24fdc9e26467da18
musti17 <mustafazahid167@gmail.com>
2023-06-18 14:42:45 +0500

    Producer Relationship with movie added

--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace eTickets.Models
 {
@@ -11,5 +12,8 @@ namespace eTickets.Models
         public string FullName { get; set; }
 
         public string Bio { get; set; }
+
+        //Relationships
+        public List<Movie>  Movies { get; set; }  
     }
 }
