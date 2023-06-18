9aa13d27c3854ebda506f53f46d4aba8947d6747
musti17 <mustafazahid167@gmail.com>
2023-06-18 12:39:37 +0500

    Movie Model modified

--- eTickets/Models/Movie.cs
+++ eTickets/Models/Movie.cs
@@ -1,6 +1,23 @@
-﻿namespace eTickets.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
 {
     public class Movie
     {
+        [Key]
+        public int MovieId { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public MovieCategory MovieCategory { get; set; }
     }
 }
