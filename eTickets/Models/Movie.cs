5111779db88683f75a8d741b24badf0e4eff2757
musti17 <mustafazahid167@gmail.com>
2023-06-18 15:06:06 +0500

    Join Table for Many to Many Relationship added

--- eTickets/Models/Movie.cs
+++ eTickets/Models/Movie.cs
@@ -1,6 +1,8 @@
 ﻿using eTickets.Data.Enums;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eTickets.Models
 {
@@ -20,5 +22,18 @@ namespace eTickets.Models
         public DateTime EndDate { get; set; }
 
         public MovieCategory MovieCategory { get; set; }
+
+        //Relationships
+        public List<Actor_Movie> Actor_Movies { get; set; }
+
+        //Cinema
+        public int CinemaId { get;set; }
+        [ForeignKey("CinemaId")]
+        public Cinema Cinema { get; set; }
+
+        //Producer
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerId")]
+        public Producer Producer { get; set; }
     }
 }
