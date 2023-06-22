aa21bc8863a02a858e1807d1c0f75f197410d832
musti17 <mustafazahid167@gmail.com>
2023-06-22 20:47:35 +0500

    Display Notations Added in Actor Model

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -7,10 +7,14 @@ namespace eTickets.Models
     {
         [Key]
         public int ActorId { get; set; }
+        
+        [Display(Name = "Profile Picture URL")]        
         public string ProfilePictureURL { get; set; }
 
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
 
+        [Display(Name = "Biography")]
         public string Bio { get; set; }
 
         public List<Actor_Movie> Actor_Movies { get; set; }
