6bada68008a35ec3f2af9ec47a23d79d88a47872
musti17 <mustafazahid167@gmail.com>
2023-06-24 17:40:25 +0500

    Movies View Added

--- eTickets/Models/Movie.cs
+++ eTickets/Models/Movie.cs
@@ -15,6 +15,7 @@ namespace eTickets.Models
         public string Description { get; set; }
 
         public double Price { get; set; }
+        
         public string ImageURL { get; set; }
 
         public DateTime StartDate { get; set; }
