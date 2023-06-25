2952b49f88ae9d72fbfd4e1a102ade09473a9c4c
musti17 <mustafazahid167@gmail.com>
2023-06-25 17:15:28 +0500

    Methods in IActorsService Added

--- eTickets/Data/Services/IActorsService.cs
+++ eTickets/Data/Services/IActorsService.cs
@@ -1,6 +1,19 @@
-﻿namespace eTickets.Data.Services
+﻿using eTickets.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace eTickets.Data.Services
 {
     public interface IActorsService
     {
+        IEnumerable<Actor> GetActors();
+
+        Actor GetActor(int id);
+
+        void Add(Actor actor);
+
+        Actor UpdateActor(int id,Actor newActor);
+
+        void DeleteActor(int id);
     }
 }
