6ad00dc163775f64445a599e139e09061516c8cb
musti17 <mustafazahid167@gmail.com>
2023-06-28 13:15:06 +0500

    Actor Model Requirements/Notations updated

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -8,13 +8,17 @@ namespace eTickets.Models
         [Key]
         public int ActorId { get; set; }
         
-        [Display(Name = "Profile Picture")]        
+        [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage ="Profile Picture is Required")]
         public string ProfilePictureURL { get; set; }
 
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage ="Full Name is Required")]
+        [StringLength(50,MinimumLength =3,ErrorMessage ="Full Name must be between 3 and 50")]
         public string FullName { get; set; }
 
         [Display(Name = "Biography")]
+        [Required(ErrorMessage ="Biography is required")]
         public string Bio { get; set; }
 
         public List<Actor_Movie> Actor_Movies { get; set; }
